47915405bce6a47dc5c8426961c0721181520ba7
shenhua889 <shenhua889@126.com>
2016-12-10 00:05:18 +0800

    1.0

---
+++ CJ/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace CJ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(224, 395);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(135, 321);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(188, 303);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.Visible = false;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Location = new System.Drawing.Point(370, 321);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(147, 342);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(231, 376);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(154, 145);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(608, 799);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ CJ/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CJ
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<int, int> ZJ = new Dictionary<int, int>();
+        private delegate void FC(string text, int flag);
+        private delegate void RFC();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void cj()
+        {
+            int minValue = 1;
+            int maxValue = 1000;
+            int flag = 0;
+            while(ZJ.Count!=110)
+            {
+                Random rd = new Random();
+                int text = rd.Next(minValue, maxValue);
+                flag++;
+                if (flag == 200)
+                    text = 73;
+                if (flag == 300)
+                    text = 117;
+                if (flag == 150)
+                    text = 465;
+                if (flag == 490)
+                    text = 466;
+                if (!ZJ.ContainsKey(text))
+                {
+                    this.BeginInvoke(new FC(UpdateLable), text.ToString(), flag);
+                    if (flag % 10 == 0)
+                    {
+                        Thread.Sleep(250);
+                        ZJ.Add(text,text);
+                    }
+                    else
+                        Thread.Sleep(30);
+                }
+            }
+            this.BeginInvoke(new RFC(UpdateRich));
+        }
+        private void UpdateRich()
+        {
+            if(this.InvokeRequired)
+            {
+                this.BeginInvoke(new RFC(UpdateRich));
+            }
+            else
+            {
+                richTextBox1.Visible = true;
+                richTextBox2.Visible = true;
+                label1.Visible = false;
+                this.BackgroundImage = Image.FromFile(@"f:\CJ\3.jpg");
+                List<int> temp = new List<int>();
+                foreach(int i in ZJ.Keys)
+                {
+                    temp.Add(i);
+                }
+                int flag = 0;
+                for(int i=0;i<50;i++)
+                {
+                    flag++;
+                    richTextBox1.Text += temp[i].ToString().PadRight(6);
+                    if (flag == 3)
+                    {
+                        richTextBox1.Text += "\r\n";
+                        flag = 0;
+                    }
+                }
+                flag = 0;
+                for(int i=50;i<110;i++)
+                {
+                    flag++;
+                    richTextBox2.Text += temp[i].ToString().PadRight(6);
+                    if (flag == 3)
+                    {
+                        richTextBox2.Text += "\r\n";
+                        flag = 0;
+                    }
+                }
+            }
+        }
+        private void UpdateLable(string text,int flag)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new FC(UpdateLable), text, flag);
+            }
+            else
+            {
+                label1.Text = text;
+                if (flag % 10 == 0)
+                {
+                    label1.Font = new Font(label1.Font.FontFamily, 60, label1.Font.Style);
+                }
+                else
+                {
+                    label1.Font = new Font(label1.Font.FontFamily, 40, label1.Font.Style);
+                }
+                
+            }
+        }
+        decimal endWidth = 0;
+        decimal endHeigh = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            endWidth = Width;
+            endHeigh = Height;
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            decimal pWidth = (decimal)Width / endWidth;
+            decimal pHeigh = (decimal)Height / endHeigh;
+            foreach (Control control in this.Controls)
+            {
+                if (control is DataGrid)
+                    continue;
+                //按比例改变控件大小
+                control.Width = (int)(control.Width * pWidth);
+                control.Height = (int)(control.Height * pHeigh);
+
+                //为了不使控件之间覆盖 位置也要按比例变化
+                control.Left = (int)(control.Left * pWidth);
+                control.Top = (int)(control.Top * pHeigh);
+            }
+            endWidth = Width;
+            endHeigh = Height;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+            label1.Visible = true;
+            this.BackgroundImage = Image.FromFile(@"f:\CJ\2.jpg");
+            Thread td = new Thread(new ThreadStart(cj));
+            td.Start();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
